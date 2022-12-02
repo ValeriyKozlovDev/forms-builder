d52f68cb1be9b7547e9213865e3a69bb64c5c0e7
Valeriy Kozlov <Valera.31.07.9.9@gmail.com>
2022-12-02 11:24:44 +0200

    added drag&drop

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,23 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { FormElementsComponent } from './components/form-elements/form-elements.component'
+import { DragDropModule } from '@angular/cdk/drag-drop';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FormElementsComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatSlideToggleModule,
+    DragDropModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/form-elements/form-elements.component.ts
@@ -0,0 +1,29 @@
+import { Component } from '@angular/core';
+import { CdkDragDrop, moveItemInArray, transferArrayItem } from '@angular/cdk/drag-drop';
+
+
+@Component({
+  selector: 'app-form-elements',
+  templateUrl: './form-elements.component.html',
+  styleUrls: ['./form-elements.component.css']
+})
+export class FormElementsComponent {
+  selectedElements = ['Input label'];
+
+  listElements = ['Input', 'Checkbox', 'Button', 'Select'];
+
+  drop(event: CdkDragDrop<string[]>) {
+    if (event.previousContainer === event.container) {
+      moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
+    } else {
+      transferArrayItem(
+        event.previousContainer.data,
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex,
+      );
+    }
+  }
+}
+
+
